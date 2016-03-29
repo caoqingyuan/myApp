605be9d1972ea78a2ac5e1685faa6c0192ece6ed
caoliangcheng <1063065295@qq.com>
2016-03-29 09:56:12 +0800

    submit project source code

---
+++ AddToDoItemViewController.m
@@ -0,0 +1,49 @@
+//
+//  AddToDoItemViewController.m
+//  myApp
+//
+//  Created by My_Mac on 16/3/18.
+//  Copyright © 2016年 My_Mac. All rights reserved.
+//
+
+#import "AddToDoItemViewController.h"
+
+@interface AddToDoItemViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
+
+@end
+
+@implementation AddToDoItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if (sender != self.doneButton) return;
+    if (self.textField.text.length > 0) {
+        self.toDoItem = [[TodoItem alloc] init];
+        self.toDoItem.itemName = self.textField.text;
+        self.toDoItem.completed = NO;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
